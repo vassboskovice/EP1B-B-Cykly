320ef9a0cb0e8c5029bd199413d8788257383e19
Jiří Hlaváček <jirka@jhlavacek.cz>
2024-05-07 08:40:51 +0200

    do-loop while

--- EP1B-B-Cykly/Program.vb
+++ EP1B-B-Cykly/Program.vb
@@ -3,12 +3,14 @@ Imports System
 Module Program
   Sub Main(args As String())
     Dim cislo As Integer
+    Dim pocet As Integer = 0
 
     Do
       Console.Write("Zadej èíslo: ")
       cislo = Console.ReadLine
-      Console.WriteLine($"Zadané èíslo je {cislo}")
-    Loop
+      pocet = pocet + 1
+      Console.WriteLine($"{pocet}. zadané èíslo je {cislo}")
+    Loop While pocet < 10
 
   End Sub
 End Module
